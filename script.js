98a879fcdc1a2da847f13f2a0cc45200c0c1f83f
koded <korexcoded@gmail.com>
2023-12-13 21:28:42 -0600

    modified files

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function changeColors(){
 
   document.getElementsByTagName("body")[0].style.background = colors[index++];
 
-  if(index > colors.length -1 )
+
+  if(index > colors.length -1)
   index = 0;
 }
